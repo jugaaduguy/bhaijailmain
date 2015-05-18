b6f0257a347f30eca80440b04c6f159996ee4420
neo01124 <mr.unwell2006@gmail.com>
2015-05-18 22:23:03 +0200

    audio added

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-$('image_container').hide()
-var aud = $('#audio_player');
-aud.oncanplaythrough = function() {
-	$('image_container').show()
-};
+// $('image_container').hide()
+// var aud = $('#audio_player');
+// aud.oncanplaythrough = function() {
+// 	$('image_container').show()
+// };
